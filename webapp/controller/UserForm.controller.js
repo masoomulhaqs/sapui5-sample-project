e02eccab6c09adee3e13d3116938db9bf36a9a31
Masoom Ul haq Shaik <masoomulhaqs@gmail.com>
2019-02-06 09:48:45 +0000

    JSON Data Example

--- webapp/controller/UserForm.controller.js
+++ webapp/controller/UserForm.controller.js
@@ -8,6 +8,23 @@ sap.ui.define([
 	return Controller.extend("sapui5-sample-project.sapui5-sample-project.controller.UserForm", {
 
 		onInit: function () {
+			// JSON sample data
+			// var data = {
+			// 	"name": "Masoom Ul Haq S",
+			// 	"email": "masoomulhaqs@gmail.com",
+			// 	"location": "Bangalore",
+			// 	"portfolio": "www.masoomulhaq.in"
+			// };
+			// var oModel = new JSONModel(data);
+			// set the data for the model
+			// oModel.setData(data);
+			// set the model to the core
+			var oModel = new JSONModel("model/UserData.json");
+			// this.getView().setModel(oModel);
+			this.getView().setModel(oModel);
+		},
+		onPress: function(evt) {
+			window.console.log(evt);
 		}
 	});
 

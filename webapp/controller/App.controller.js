386bec500e4e2580937fe36605e98e938d526156
Masoom Ul haq Shaik <masoomulhaqs@gmail.com>
2019-02-06 08:52:50 +0000

    Tutorial additions

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -18,10 +18,39 @@ sap.ui.define([
 		},
 
 		onSelectTab: function (event) {
-			var label = this.byId('labelId');
+			// var label = this.byId('labelId');
 			var tab = event.getParameter('item');
+			window.console.log(tab.getText());
 
-			label.setText(tab.getText());
+			// label.setText(tab.getText());
+		},
+		
+		navigateTo: function(evt) {
+			window.console.log(evt.getSource());
+			// var label = this.byId('labelId');
+			// this.getOwnerComponent().getRouter().navTo(label);
+		},
+		navigateToHome: function(evt) {
+			window.console.log("home");
+			this.getOwnerComponent().getRouter().navTo("home");
+		},
+		navigateToCarousel: function(evt) {
+			window.console.log("page1");
+			this.getOwnerComponent().getRouter().navTo("carousel");
+		},
+		navigateToPage1: function(evt) {
+			window.console.log("page1");
+			this.getOwnerComponent().getRouter().navTo("page1");
+		},
+		navigateToPage2: function(evt) {
+			window.console.log("page2");
+			this.getOwnerComponent().getRouter().navTo("page2");
+		},
+		navigateToUserForm: function(evt) {
+			this.getOwnerComponent().getRouter().navTo("userform");
+		},
+		navigateToAppConfig: function(evt) {
+			this.getOwnerComponent().getRouter().navTo("appconfig");
 		}
 		
 	});
---
+++ webapp/controller/AppConfig.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	'jquery.sap.global',
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/model/json/JSONModel'
+], function (jQuery, Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sapui5-sample-project.sapui5-sample-project.controller.AppConfig", {
+
+		onInit: function () {
+		}
+	});
+
+});
